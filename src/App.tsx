b5b86ecfe6d87e7c5e80ebee967e6dcf6411ad29
Angel Martinez <angel.martineztrc@gmail.com>
2022-02-09 22:54:36 -0600

    feat:  add background image

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { HelloWorld } from '@/components/HelloWorld';
 
 const App = () => {
   return (
-    <div>
-      <HelloWorld />
-      <h1 className="font-inter text-black">Hello, World!</h1>
+    <div className="before:bg-daytime-banner relative min-h-screen before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-cover before:bg-center before:bg-no-repeat before:brightness-[.6] before:content-['']">
+      <div className="relative">
+        <HelloWorld />
+        <h1 className="font-inter text-white">Hello, World!</h1>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{jsx,js,tsx}'],
+  mode: 'jit',
   theme: {
     extend: {
       colors: {
         jet: '#303030'
+      },
+      backgroundImage: {
+        'daytime-banner': "url('/src/assets/bg-image-daytime.jpg')"
       }
     },
     fontFamily: {
